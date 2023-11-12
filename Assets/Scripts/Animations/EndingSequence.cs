9072c1903468849bfb0fac371107c417f1f0b905
강승연 <syeon.dev@gmail.com>
2023-11-12 15:21:37 +0900

    엔딩씬

--- Assets/Scripts/Animations/EndingSequence.cs
+++ Assets/Scripts/Animations/EndingSequence.cs
@@ -9,9 +9,20 @@ public class EndingSequence : MonoBehaviour
     public GameObject Kid2;
     public GameObject Kid3;
     public GameObject Logo;
+    public GameObject Candy;
     void Start()
     {
-        
+        Sequence s = DOTween.Sequence();
+
+        s.Append(Kid1.transform.DOMoveX(0f, 3f).SetEase(Ease.InBounce));
+        s.Append(Logo.transform.DOMoveX(3.0f, 2f).SetEase(Ease.InOutBack));
+        s.Append(Witch.transform.DOMoveX(-3.0f, 5f).SetEase(Ease.OutCirc));
+        s.Append(Candy.transform.DOMoveY(-1.87f, 2f).SetEase(Ease.InSine));
+        s.Append(Kid1.transform.DOMoveY(Kid1.transform.position.y + 1f, 0.5f).SetLoops(2, LoopType.Yoyo));
+        s.Append(Kid2.transform.DOMoveY(Kid2.transform.position.y + 1f, 0.5f).SetLoops(2, LoopType.Yoyo));
+        s.Append(Kid3.transform.DOMoveY(Kid3.transform.position.y + 1f, 0.5f).SetLoops(2, LoopType.Yoyo));
+
+        s.Play();
     }
 
 }
