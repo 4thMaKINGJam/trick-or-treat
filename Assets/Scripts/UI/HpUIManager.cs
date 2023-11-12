84e3c684e00ca42a6f2ab1bed58085500a4cd39e
jaeseo <jaeserrr@ewhain.net>
2023-11-12 10:18:36 +0900

    [FEAT] Stage2 background

--- Assets/Scripts/UI/HpUIManager.cs
+++ Assets/Scripts/UI/HpUIManager.cs
@@ -2,14 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+
 
 public class HpUIManager : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI hpCnt; // hp °³¼ö ui
+    [SerializeField]
+    private GameObject healthImagePrefab;
+    private float spacing = 0.8f; // Adjust this value to set the spacing between health images
 
-    // Update is called once per frame
-    void Update()
+    public void Start()
     {
-        hpCnt.text = GameManager.playerHp.ToString();
+        int hp = GameManager.playerHp;
+
+        for (int i = 0; i < hp; i++)
+        {
+            float xPos = transform.position.x + i * spacing;
+            Vector3 spawnPosition = new Vector3(xPos, 0, 0);
+            //Instantiate(healthImagePrefab, spawnPosition, Quaternion.identity);
+
+            // Instantiate healthImagePrefab as a child of the current GameObject
+            GameObject healthImageInstance = Instantiate(healthImagePrefab, spawnPosition, Quaternion.identity, transform);
+
+            // Optionally, you can rename the instantiated object for clarity
+            healthImageInstance.name = "HealthImage" + i;
+        }
     }
 }
